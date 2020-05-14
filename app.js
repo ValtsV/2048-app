a482ca8b8eb0bebc7b13a3b2e183ab7ca1cd14e4
Valts Vilcans <valts03@inbox.lv>
2020-05-14 14:57:18 +0200

    [added pointerdown listener, register move]

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ for (let index = 0; index < 16; index++) {
 }
 
 console.log(squares);
+
 const emptySquares = Object.keys(squares).filter((key) => {
   if (!squares[key]) {
     return key;
@@ -31,4 +32,21 @@ const addNumber = () => {
   console.log(emptySquares);
 };
 
+const addMouseMoveListener = () => {
+  document.addEventListener("pointermove", (event) => {
+    console.log(event.pageX); // compar pagex to initial coord, when difference reaches 100px or smth, fire
+  });
+  const x = event.clientX;
+  const y = event.clientY;
+  console.log(x, y);
+};
+
 document.getElementById("start").addEventListener("click", addNumber);
+
+document
+  .getElementById("main-block")
+  .addEventListener("pointerdown", addMouseMoveListener);
+
+// const cont = document.querySelector(".game-container");
+
+// cont.onmousedown = addMouseMoveListener;
