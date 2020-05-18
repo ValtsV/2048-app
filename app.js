d400b0ea288375529335e342173cae9eaf989cb1
Valts Vilcans <valts03@inbox.lv>
2020-05-18 14:01:32 +0200

    [added method for swap out square classes for animation]

--- app.js
+++ app.js
@@ -1,310 +1,240 @@
 // Make array of objects, where each object is a square
 
-const squares = [];
-for (let index = 0; index < 16; index++) {
-  squares.push({
-    id: index + 1,
-    value: 0,
-    posChange: "none",
-    changeDirection: "none",
+// elem.addEventListener("click", () => {
+//   const squery = document.getElementById("s16");
+//   squery.classList.add("up1");
+//   setTimeout(() => {
+//     squery.classList.toggle("transition");
+//   }, 10);
+  
+
+  const elem = document.getElementById("anim");
+  elem.addEventListener("click", () => {
+    const squery = document.getElementById("s16");
+    squery.classList.add("square-bottom");
+    squery.classList.remove("square");
+    setTimeout(() => {
+      squery.classList.toggle("transition");
+    }, 10);
+    // squery.classList.add("up2");
+    console.log("added class");
+
+    console.log("added transition");
   });
-}
-const isPlaying = false;
 
-const updateGameboard = () => {
-  // reads squares and updates gameboard accordingly
-  console.log("gameboard rendered");
-};
+  const squares = [];
+  for (let index = 0; index < 16; index++) {
+    squares.push({
+      id: index + 1,
+      value: 0,
+      posChange: "none",
+      changeDirection: "none",
+    });
+  }
+  const isPlaying = false;
 
-const resetGameboard = () => {
-  // resets the squares objects
-  console.log("squares reset");
-};
+  const updateGameboard = () => {
+    // reads squares and updates gameboard accordingly
+    console.log("gameboard rendered");
+  };
 
-const startGame = () => {
-  // Checks which squares are free and adds randomly a number
-  const nextSquare =
-    emptySquares[Math.floor(Math.random() * Math.floor(emptySquares.length))];
+  const resetGameboard = () => {
+    // resets the squares objects
+    console.log("squares reset");
+  };
 
-  const square = document.getElementById(`s${nextSquare}`);
-  squares[nextSquare] = 0;
-  square.textContent = "2";
-  console.log("id of square:" + `s${nextSquare}`);
+  const startGame = () => {
+    // Checks which squares are free and adds randomly a number
+    const nextSquare =
+      emptySquares[Math.floor(Math.random() * Math.floor(emptySquares.length))];
 
-  const x = emptySquares.findIndex((el) => el == nextSquare);
-  console.log("index is:" + x);
+    const square = document.getElementById(`s${nextSquare}`);
+    squares[nextSquare] = 0;
+    square.textContent = "2";
+    console.log("id of square:" + `s${nextSquare}`);
 
-  emptySquares.splice(x, 1);
+    const x = emptySquares.findIndex((el) => el == nextSquare);
+    console.log("index is:" + x);
 
-  console.log(emptySquares);
+    emptySquares.splice(x, 1);
 
-  squares[nextSquare] = 2;
+    console.log(emptySquares);
 
-  console.log(squares);
+    squares[nextSquare] = 2;
 
-  isPlaying = true;
-};
+    console.log(squares);
 
-const calcSquares = (a, b, c, d) => {
-  if (a === b) {
-    a.value * 2;
-    a.posChange + 0;
-    b.posChange + 1;
-    c.posChange + 1;
-    d.posChange + 1;
-    if (c === d) {
+    isPlaying = true;
+  };
+
+  const calcSquares = (a, b, c, d) => {
+    if (a.value === b.value) {
+      a.value * 2;
+      a.posChange + 0;
+      b.posChange + 1;
+      c.posChange + 1;
+      d.posChange + 1;
+      if (c.value === d.value) {
+        c.value * 2;
+        d.posChange + 1;
+      }
+    } else if (a.value === c.value && b.value === 0) {
+      a.value * 2;
+      c.posChange + 2;
+      d.posChange + 2;
+    } else if (a.value === d.value && b.value === 0 && c.value === 0) {
+      a.value * 2;
+      d.posChange + 3;
+    } else if (b.value === c.value) {
+      if (a.value === 0) {
+        b.posChange + 1;
+        c.posChange + 1;
+        d.posChange + 1;
+      }
+      b.value * 2;
+      c.posChange + 1;
+      d.posChange + 1;
+    } else if (b.value === d.value && c.value === 0) {
+      if (a.value === 0) {
+        b.posChange + 1;
+        c.posChange + 1;
+        d.posChange + 1;
+      }
+      b.value * 2;
+      d.posChange + 2;
+    } else if (c.value === d.value) {
+      if (a.value === 0) {
+        b.posChange + 1;
+        c.posChange + 1;
+        d.posChange + 1;
+      }
+      if (b.value === 0) {
+        c.posChange + 1;
+        d.posChange + 1;
+      }
       c.value * 2;
       d.posChange + 1;
     }
-  } else if (a === c && b === 0) {
-    a.value * 2;
-    c.posChange + 2;
-    d.posChange + 2;
-  } else if (a === d && b === 0 && c === 0) {
-    a.value * 2;
-    d.posChange + 3;
-  } else if (b === c) {
-    if (a === 0) {
-      b.posChange + 1;
-      c.posChange + 1;
-      d.posChange + 1;
-    }
-    b.value * 2;
-    c.posChange + 1;
-    d.posChange + 1;
-  } else if (b === d && c === 0) {
-    if (a === 0) {
-      b.posChange + 1;
-      c.posChange + 1;
-      d.posChange + 1;
-    }
-    b.value * 2;
-    d.posChange + 2;
-  } else if (c === d) {
-    if (a === 0) {
-      b.posChange + 1;
-      c.posChange + 1;
-      d.posChange + 1;
-    }
-    if (b === 0) {
-      c.posChange + 1;
-      d.posChange + 1;
-    }
-    c.value * 2;
-    d.posChange + 1;
-  }
-};
+  };
 
-const checkForMovement = () => {
-  while (isPlaying) {
-    document.getElementById("start").addEventListener("click", addNumber);
+  const checkForMovement = () => {
+    while (isPlaying) {
+      document.getElementById("start").addEventListener("click", addNumber);
 
-    const gameBoard = document.getElementById("main-block");
+      const gameBoard = document.getElementById("main-block");
 
-    // ////////// checks direction
+      // ////////// checks direction
 
-    let pressedDown = false;
+      let pressedDown = false;
 
-    let x;
+      let x;
 
-    const diff = {
-      left: 0,
-      right: 0,
-      up: 0,
-      down: 0,
-    };
+      const diff = {
+        left: 0,
+        right: 0,
+        up: 0,
+        down: 0,
+      };
 
-    const func3 = () => {
-      pressedDown = false;
-    };
-
-    // 4. runs func for pointermove
-    const func2 = () => {
-      // calculates difference between starting point and current point
-      diff.left = x - event.pageX;
-      diff.right = event.pageX - x;
-      diff.up = x - event.pageY;
-      diff.down = event.pageY - x;
-
-      if (pressedDown) {
-        // returns empty string or first value that reaches 100
-        const dirrecc = Object.keys(diff)
-          .filter((key) => diff[key] > 100)
-          .toString();
-
-        switch (
-          dirrecc // outputs dirrection
-        ) {
-          case "left":
-            console.log("left");
-            break;
-          case "right":
-            console.log("right");
-            break;
-          case "up":
-            console.log("up");
-            break;
-          case "down":
-            console.log("down");
-            break;
-          default:
-            console.log("no move");
-            break;
-        }
-      } else {
-        // stops checking for coords after pointer is lifted
-        gameBoard.removeEventListener("pointermove", func2);
-        gameBoard.removeEventListener("pointerup", func3, false);
+      const func3 = () => {
         pressedDown = false;
-      }
-    };
+      };
 
-    const func = () => {
-      pressedDown = true;
-      gameBoard.addEventListener("pointerup", func3, false);
-      console.log(event.pageX);
-      x = event.pageX;
+      // 4. runs func for pointermove
+      const func2 = () => {
+        // calculates difference between starting point and current point
+        diff.left = x - event.pageX;
+        diff.right = event.pageX - x;
+        diff.up = x - event.pageY;
+        diff.down = event.pageY - x;
 
-      //  3. adds listener for pointermove
-      gameBoard.addEventListener("pointermove", func2);
-    };
+        if (pressedDown) {
+          // returns empty string or first value that reaches 100
+          const dirrection = Object.keys(diff)
+            .filter((key) => diff[key] > 100)
+            .toString();
 
-    // 1.Adds listener for a click
+          switch (
+            dirrection // outputs dirrection
+          ) {
+            case "left":
+              console.log("left");
+              swapSquareClass(dirrection);
 
-    gameBoard.addEventListener("pointerdown", func, false);
-  }
-};
+              calcSquares(squares[0], squares[4], squares[8], squares[12]);
+              break;
+            case "right":
+              console.log("right");
+              swapSquareClass(dirrection);
+              break;
+            case "up":
+              console.log("up");
+              swapSquareClass(dirrection);
+              break;
+            case "down":
+              console.log("down");
+              swapSquareClass(dirrection);
+              break;
+            default:
+              console.log("no move");
+              break;
+          }
+        } else {
+          // stops checking for coords after pointer is lifted
+          gameBoard.removeEventListener("pointermove", func2);
+          gameBoard.removeEventListener("pointerup", func3, false);
+          pressedDown = false;
+        }
+      };
 
-// // Make squares
+      const func = () => {
+        pressedDown = true;
+        gameBoard.addEventListener("pointerup", func3, false);
+        console.log(event.pageX);
+        x = event.pageX;
 
-// const squares = {};
-// for (let index = 0; index < 16; index++) {
-//   squares["sq" + (index + 1).toString()] = 0;
-// }
+        //  3. adds listener for pointermove
+        gameBoard.addEventListener("pointermove", func2);
+      };
 
-// console.log(squares);
+      // 1.Adds listener for a click
 
-// const emptySquares = Object.keys(squares).map((key) => {
-//   if (squares[key] === 0) {
-//     return key.slice(2);
-//   }
-// });
+      gameBoard.addEventListener("pointerdown", func, false);
+    }
+  };
 
-// console.log(emptySquares);
+  // swaps square class for animation to work
 
-// const addNumber = () => {
-//   // Checks which squares are free and adds randomly a number
-//   const nextSquare =
-//     emptySquares[Math.floor(Math.random() * Math.floor(emptySquares.length))];
+  const swapSquareClass = (direction) => {
+    const squares = document.getElementsByClassName("square");
+    switch (direction) {
+      case "left":
+        Array.from(squares).forEach((square) => {
+          square.classList.add("square-left");
+          square.classList.remove("square");
+        });
+        break;
+      case "right":
+        Array.from(squares).forEach((square) => {
+          square.classList.add("square-right");
+          square.classList.remove("square");
+        });
+        break;
+      case "up":
+        Array.from(squares).forEach((square) => {
+          square.classList.add("square-up");
+          square.classList.remove("square");
+        });
+        break;
+      case "down":
+        Array.from(squares).forEach((square) => {
+          square.classList.add("square-left");
+          square.classList.remove("square");
+        });
+        break;
 
-//   const square = document.getElementById(`s${nextSquare}`);
-//   squares[nextSquare] = 0;
-//   square.textContent = "2";
-//   console.log("id of square:" + `s${nextSquare}`);
-
-//   const x = emptySquares.findIndex((el) => el == nextSquare);
-//   console.log("index is:" + x);
-
-//   emptySquares.splice(x, 1);
-
-//   console.log(emptySquares);
-
-//   squares[nextSquare] = 2;
-
-//   console.log(squares);
-// };
-
-// document.getElementById("start").addEventListener("click", addNumber);
-
-// const gameBoard = document.getElementById("main-block");
-
-// // ////////// checks direction
-
-// let pressedDown = false;
-
-// let x;
-
-// const diff = {
-//   left: 0,
-//   right: 0,
-//   up: 0,
-//   down: 0,
-// };
-
-// const func3 = () => {
-//   pressedDown = false;
-// };
-
-// // 4. runs func for pointermove
-// const func2 = () => {
-//   // calculates difference between starting point and current point
-//   diff.left = x - event.pageX;
-//   diff.right = event.pageX - x;
-//   diff.up = x - event.pageY;
-//   diff.down = event.pageY - x;
-
-//   if (pressedDown) {
-//     // returns empty string or first value that reaches 100
-//     const dirrecc = Object.keys(diff)
-//       .filter((key) => diff[key] > 100)
-//       .toString();
-
-//     switch (
-//       dirrecc // outputs dirrection
-//     ) {
-//       case "left":
-//         console.log("left");
-//         break;
-//       case "right":
-//         console.log("right");
-//         break;
-//       case "up":
-//         console.log("up");
-//         break;
-//       case "down":
-//         console.log("down");
-//         break;
-//       default:
-//         console.log("no move");
-//         break;
-//     }
-//   } else {
-//     // stops checking for coords after pointer is lifted
-//     gameBoard.removeEventListener("pointermove", func2);
-//     gameBoard.removeEventListener("pointerup", func3, false);
-//     pressedDown = false;
-//   }
-// };
-
-// const func = () => {
-//   pressedDown = true;
-//   gameBoard.addEventListener("pointerup", func3, false);
-//   console.log(event.pageX);
-//   x = event.pageX;
-
-//   //  3. adds listener for pointermove
-//   gameBoard.addEventListener("pointermove", func2);
-// };
-
-// // 1.Adds listener for a click
-
-// gameBoard.addEventListener("pointerdown", func, false);
-
-// // UP
-
-// const squaresPos = [...squares];
-// console.log(squaresPos);
-
-// const { sq1, sq2, sq3, sq4 } = squares;
-// const row1 = [sq1, sq2, sq3, sq4];
-// const { sq5, sq6, sq7, sq8 } = squares;
-// const row2 = [sq5, sq6, sq7, sq8];
-// const { sq9, sq10, sq11, sq12 } = squares;
-// const row3 = [sq9, sq10, sq11, sq12];
-// const { sq13, sq14, sq15, sq16 } = squares;
-// const row4 = [sq13, sq14, sq15, sq16];
-
-// const funct = (arr) => {
-//   if (arr[0]) {
-//   }
-// };
+      default:
+        break;
+    }
+  };
+});
